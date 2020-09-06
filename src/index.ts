b7e915fbfc5f1e4dacf6db7c11381b66b07c17a5
Leonard Scheidemantel <leonard.scheidemantel@googlemail.com>
2020-09-06 13:28:07 +0200

    Add sleep as export statement to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { EOSIOStreamTokenizer } from './protocol/stream/tokenizer';
 export { EOSIOStreamConsoleDebugger } from "./protocol/stream/debugger";
 export { EOSIOP2PClientConnection } from './protocol/connection';
 export { GoAwayMessage, HandshakeMessage, SyncRequestMessage } from './protocol/messages';
+export { sleep }  from './includes/utils';
